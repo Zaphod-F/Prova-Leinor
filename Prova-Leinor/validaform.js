678a42079907729077a96a7780fda285b7faacc8
Flavio <flavioluismilani@hotmail.com>
2019-05-29 16:25:56 -0300

    Atividade 4 - Finalização da Prova

--- Prova-Leinor/validaform.js
+++ Prova-Leinor/validaform.js
@@ -1,29 +1,36 @@
 var enviarMensagem = document.querySelector("#bt-enviar-mensagem");
 
 var formulario = document.querySelector("#formulario");
+$("#formulario").attr("hidden", false);
 
 enviarMensagem.addEventListener("click", function(event){
     event.preventDefault();
-    
-    validacaoNomeEmail();
-    
+
+    validacaoNome();
+
+    validaEmail();
+
     validacaoRadio();
-    
+
     validacaoCheck();
-    
+
     validaMensagem();
-    
+
+    escondeForm();
 });
 
-function validacaoNomeEmail() {
-    var nome = formulario.nome;
-    var email = formulario.email;
+function validacaoNome() {
+    var nome = $("#nome").val();
 
-    if (nome.value == "") {
+    var qtdPalavras = nome.split(/\S+/).length -1;
+
+    if (nome.value == "" || qtdPalavras <2) {
         alert("Insira um nome válido.");
         return false;
     }
-
+  }
+  function validaEmail(){
+    var email = formulario.email;
     if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
         alert("Insira um email válido");
         return false;
@@ -32,7 +39,7 @@ function validacaoNomeEmail() {
 
 function validacaoRadio(){
     var radio = formulario.disponibilidade;
-    
+
     for (i = 0; i<radio.length;i++){
         if (radio[i].checked){
             return true;
@@ -44,7 +51,7 @@ function validacaoRadio(){
 
 function validacaoCheck(){
     var check = formulario.interesses;
-    
+
     for (i = 0; i<check.length;i++){
         if (check[i].checked){
             return true;
@@ -56,9 +63,39 @@ function validacaoCheck(){
 
 function validaMensagem(){
     var mensagem = formulario.mensagem;
-    
+
     if (mensagem.value.length < 140){
         alert("Insira uma mensagem com pelo menos 140 caracteres");
         return false;
     }
+}
+
+function mostraPersonalizar(){
+  var disponibilidade = $("#radper");
+  console.log(disponibilidade);
+  disponibilidade.on("click",function() {
+    if(disponibilidade.is(":checked")){
+      $("#personalizar").attr("hidden",false);
+    }else{
+      $("#personalizar").attr("hidden",true);
+    }
+  });
+}
+mostraPersonalizar();
+
+function mostraOutro(){
+  var outro = $("#outro");
+  outro.on("click", function() {
+    if(outro.is(":checked")) {
+      $("#outroCampo").attr("hidden", false);
+    } else {
+      $("#outroCampo").attr("hidden", true);
+    }
+  });
+}
+mostraOutro();
+
+function escondeForm(){
+$("#formulario").attr("hidden", true);
+  $("#depois-form").attr("hidden", false);
 }
